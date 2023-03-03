efd072c152d8c6dc3469c6374b70312b506e6aa8
AlexanderZhao <zhaomengzhe@gmail.com>
2023-03-03 11:37:25 +0800

    feat(): set bracketSameLine false and singleAttributePerLine true

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,11 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSpacing: true,
+  bracketSameLine: false,
   printWidth: 140,
   proseWrap: 'preserve',
   semi: false,
+  singleAttributePerLine: true,
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'es5',
